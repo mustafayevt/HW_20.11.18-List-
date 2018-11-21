cfee5cae5a915891bd5537b38ec36dcd0222df90
Tural Mustafayev <mustafayevt@users.noreply.github.com>
2018-11-22 01:52:24 +0400

    Add project files.

---
+++ HW_20.11.18(List)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_20._11._18_List_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
